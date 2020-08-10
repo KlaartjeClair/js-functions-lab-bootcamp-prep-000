c37cffd58a24275206f5279c7be5585327e9b809
Klaartje Claire <rosemarieclaire@gmail.com>
2020-08-10 18:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHollidays()
+{
+  return 'Happy holidays!';
+}
+function happyHolidaysTo()
+{
+  return `Happy happyHolidaysTo ${name}`;
+}
+  function happyCustomHolidayTo()
+  { 
+    return `Happy ${holiday}, ${name}`;
+    }
+  function holidayContdown()
+  {
+    return ``
+  }
